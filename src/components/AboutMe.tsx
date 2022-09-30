81a4e2d20ab7ce23e1c8be9775e4d56ed1cd9f22
Raynne-Andrade <raynne.andrade.santos@gmail.com>
2022-09-30 15:34:13 -0300

    docs: :bulb: remove commented

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,11 +6,15 @@ const AboutMe = () => {
                 <div>
                     <img src="../assets/Raynne.png" width={"300px"} />
                 </div>
-                <div className="text-black dark:text-white self-center ">
-                    <p> 23 anos, formada em analíse e desenvolvimento de sistemas. </p>
-                    <p> Atuando como Desenvolvedor Front-end, onde lido com aplicações Web em Vue.js, Laravel com template blade. </p>
-                    <p> Possuo conhecimentos sólidos em: HTML5, CSS3, JavaScript, SASS, Figma, Bootstrap, Git, ReactJs e VueJs.</p>
-                    <p> Busco futuramente ingressar no mundo do desenvolvimento de jogos em Java e Python. </p>
+                <div className="text-neutral-700 dark:text-white self-center text-lg font-normal
+                  max-w-4xl leading-7">
+                    <h3 className="text-3xl font-bold text-neutral-800 mb-4">
+                        Eu crio sites para pequenas empresas e empreendedores que querem crescer e crescer. 
+                    </h3>
+                    <p> Especialista em HTML, CSS e Javascript, com sólidos conhecimentos em arquitetura do ecosistema Front-End e em tecnologias com  React, VueJs, TypeScript, Web Components, design responsivo, web mobile, entre outros. </p>
+                    <p> Sou entusiasta de tecnologia, gosto de me manter atualizada sobre as tendencias e como elas podem nos auxiliar. Também gosto de livros cliches, gatinhos fofos e café. </p>
+                    <p> Busco futuramente ingressar no mundo do desenvolvimento de jogos em Javascript, Java e Python. </p>
+
                 </div>
             </div>
         </section>
